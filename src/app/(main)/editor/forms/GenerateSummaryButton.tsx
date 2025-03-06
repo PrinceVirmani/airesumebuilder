b244623d4adda266b7bb3672f26a5a54d46289de
Prince <princevirmani410@gmail.com>
2025-03-06 14:49:30 +0530

    ai integration

--- src/app/(main)/editor/forms/GenerateSummaryButton.tsx
+++ src/app/(main)/editor/forms/GenerateSummaryButton.tsx
@@ -1,61 +1,47 @@
-// import LoadingButton from "@/components/LoadingButton";
-// import { useToast } from "@/hooks/use-toast";
-// import usePremiumModal from "@/hooks/usePremiumModal";
-// import { canUseAITools } from "@/lib/permissions";
+import LoadingButton from "@/components/LoadingButton";
+import { useToast } from "@/hooks/use-toast";
+import { WandSparklesIcon } from "lucide-react";
+import { useState } from "react";
+import { generateSummary } from "./action";
 
-// import { WandSparklesIcon } from "lucide-react";
-// import { useState } from "react";
-// import { useSubscriptionLevel } from "../../SubscriptionLevelProvider";
-// import { generateSummary } from "./actions";
-// import { ResumeVlaues } from "@/lib/validation";
+interface GenerateSummaryButtonProps {
+  resumeData: ResumeVlaues;
+  onSummaryGenerated: (summary: string) => void;
+}
 
-// interface GenerateSummaryButtonProps {
-//   resumeData: ResumeVlaues;
-//   onSummaryGenerated: (summary: string) => void;
-// }
+export default function GenerateSummaryButton({
+  resumeData,
+  onSummaryGenerated,
+}: GenerateSummaryButtonProps) {
+  const { toast } = useToast();
 
-// export default function GenerateSummaryButton({
-//   resumeData,
-//   onSummaryGenerated,
-// }: GenerateSummaryButtonProps) {
-//   const subscriptionLevel = useSubscriptionLevel();
+  const [loading, setLoading] = useState(false);
 
-//   const premiumModal = usePremiumModal();
+  async function handleClick() {
+    try {
+      setLoading(true);
+      const aiResponse = await generateSummary(resumeData);
+      onSummaryGenerated(aiResponse);
+    } catch (error) {
+      console.error(error);
+      toast({
+        variant: "destructive",
+        description: "Something went wrong. Please try again.",
+      });
+    } finally {
+      setLoading(false);
+    }
+  }
 
-//   const { toast } = useToast();
-
-//   const [loading, setLoading] = useState(false);
-
-//   async function handleClick() {
-//     if (!canUseAITools(subscriptionLevel)) {
-//       premiumModal.setOpen(true);
-//       return;
-//     }
-
-//     try {
-//       setLoading(true);
-//       const aiResponse = await generateSummary(resumeData);
-//       onSummaryGenerated(aiResponse);
-//     } catch (error) {
-//       console.error(error);
-//       toast({
-//         variant: "destructive",
-//         description: "Something went wrong. Please try again.",
-//       });
-//     } finally {
-//       setLoading(false);
-//     }
-//   }
-
-//   return (
-//     <LoadingButton
-//       variant="outline"
-//       type="button"
-//       onClick={handleClick}
-//       loading={loading}
-//     >
-//       <WandSparklesIcon className="size-4" />
-//       Generate (AI)
-//     </LoadingButton>
-//   );
-// }
+  return (
+    <LoadingButton
+      variant="outline"
+      type="button"
+      onClick={handleClick}
+      loading={loading}
+    >
+      <WandSparklesIcon className="size-4" />
+      Generate (AI)
+    </LoadingButton>
+  );
+}
---
+++ src/components/LoadingButton.tsx
@@ -0,0 +1,26 @@
+import { cn } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
+import { Button } from "./ui/button";
+import { ComponentProps } from "react";
+
+interface LoadingButtonProps extends ComponentProps<typeof Button> {
+  loading: boolean;
+}
+
+export default function LoadingButton({
+  loading,
+  disabled,
+  className,
+  ...props
+}: LoadingButtonProps) {
+  return (
+    <Button
+      disabled={loading || disabled}
+      className={cn("flex items-center gap-2", className)}
+      {...props}
+    >
+      {loading && <Loader2 className="size-5 animate-spin" />}
+      {props.children}
+    </Button>
+  );
+}
